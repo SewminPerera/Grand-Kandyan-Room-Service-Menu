3df803faa725c0cbc31a8488e1920545d2a84dd1
Sewmin Perera <sewwaus2@gmail.com>
2026-07-25 16:28:17 +0530

    i

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import "./App.css";
+import HeaderSection from "./components/HeaderSection/HeaderSection";
 import BreakfastSection from "./components/BreakfastSection/BreakfastSection";
 import BreadSection from "./components/BreadSection/BreadSection";
-import DessertSection from "./components/DessertSection/DessertSection";
-import AsianSection from "./components/AsianSection/AsianSection";
-import SidesSection from "./components/SidesSection/SidesSection";
-import MainEventSection from "./components/MainEventSection/MainEventSection";
-import PizzaSection from "./components/PizzaSection/PizzaSection";
-import NightMenuSection from "./components/NightMenuSection/NightMenuSection";
 import SoupSection from "./components/SoupSection/SoupSection";
 import AppetizersSection from "./components/AppetizersSection/AppetizersSection";
-import HeaderSection from "./components/HeaderSection/HeaderSection";
+import DessertSection from "./components/DessertSection/DessertSection";
+import AsianSection from "./components/AsianSection/AsianSection";
+import PizzaSection from "./components/PizzaSection/PizzaSection";
+import NightMenuSection from "./components/NightMenuSection/NightMenuSection";
+import SidesSection from "./components/SidesSection/SidesSection";
+import MainEventSection from "./components/MainEventSection/MainEventSection";
 
 function App() {
   return (
-    <main className="menu-container">
+    <>
       <HeaderSection />
-      <BreakfastSection />
-      <BreadSection />
-      <DessertSection />
-      <AsianSection />
-      <SidesSection />
-      <MainEventSection />
-      <PizzaSection />
-      <NightMenuSection />
-      <SoupSection />
-      <AppetizersSection />
-    </main>
+      <main className="menu-container">
+        <BreakfastSection />
+        <BreadSection />
+        <SoupSection />
+        <AppetizersSection />
+        <DessertSection />
+        <AsianSection />
+        <PizzaSection />
+        <NightMenuSection />
+        <SidesSection />
+        <MainEventSection />
+      </main>
+    </>
   );
 }
 
