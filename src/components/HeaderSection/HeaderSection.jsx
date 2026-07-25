40ac59d7d6e159ae188669f76b6ca28921132681
Sewmin Perera <sewwaus2@gmail.com>
2026-07-25 17:45:31 +0530

    i

--- src/components/HeaderSection/HeaderSection.jsx
+++ src/components/HeaderSection/HeaderSection.jsx
@@ -2,6 +2,7 @@ import "./HeaderSection.css";
 import logoImage from "../../assets/images/logo.png";
 import greenChili from "../../assets/images/green-chili.png";
 import redChili from "../../assets/images/red-chili.png";
+import shrimp from "../../assets/images/shrimp.png"
 
 export default function HeaderSection() {
   return (
@@ -34,7 +35,8 @@ export default function HeaderSection() {
             Vegetarian (V)
             <img src={greenChili} alt="vegetarian" className="legend-icon" />, 
             Pork (P), Alcohol (A), Nuts (N), Spicy 
-            <img src={redChili} alt="spicy" className="legend-icon" />
+            <img src={redChili} alt="spicy" className="legend-icon" />,
+            Seafood (SF) <img src={shrimp} alt="seafood" className="legend-icon"/>
           </div>
           <div>If you are prone to food allergy or intolerance or special dietary restriction, please inform our F & B team. We will be happy to assist with your needs.</div>
           <div>All prices are subject to Service Charge and Government taxes</div>
